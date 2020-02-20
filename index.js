0b95c03947e15a23d47f31fbee5705e520405fb9
ohnewdev <ohnewdev@gmail.com>
2020-02-21 01:08:34 +0900

    add morgan,helmet,cookieParser,bodyParser

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // const express = require("express");
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 const app = express();
 
 // // respond with "hello world" when a GET request is made to the homepage
@@ -15,6 +19,23 @@ const handleHome = (req, res) => res.send("Hello from Home!!");
 
 const handleProfile = (req, res) => res.send("You are on my profile!!");
 
+const betweenHome = (req, res, next) => {
+  console.log("I'm between");
+  next();
+};
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(betweenHome);
+
+const middleware = (req, res, next) => {
+  res.send("not happening");
+};
+
 app.get("/", handleHome);
+
 app.get("/profile", handleProfile);
 app.listen(4000, handleListening);
