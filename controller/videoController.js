ea458fd1a8f264f0c08a93733f9a2e6fa0199cbf
ohnewdev <ohnewdev@gmail.com>
2020-02-23 13:33:34 +0900

    2.13

--- controller/videoController.js
+++ controller/videoController.js
@@ -1,2 +1,6 @@
-export const home = (req, res) => res.send("Home at Video");
-export const search = (req, res) => res.send("Search");
+export const home = (req, res) => res.render("home");
+export const search = (req, res) => res.render("Search");
+export const logout = (req, res) => res.render("logout");
+export const userDetail = (req, res) => res.render("userDetail");
+export const editProfile = (req, res) => res.render("editProfile");
+export const changePassword = (req, res) => res.render("changePassword");
