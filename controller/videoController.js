5154d5c54aa207831b84e77f3e016f91fc8a648a
ohnewdev <ohnewdev@gmail.com>
2020-02-25 05:09:09 +0900

    #2.17 Template Variables in Pug

--- controller/videoController.js
+++ controller/videoController.js
@@ -1,6 +1,11 @@
-export const home = (req, res) => res.render("home");
-export const search = (req, res) => res.render("Search");
-export const logout = (req, res) => res.render("logout");
-export const userDetail = (req, res) => res.render("userDetail");
-export const editProfile = (req, res) => res.render("editProfile");
-export const changePassword = (req, res) => res.render("changePassword");
+export const home = (req, res) => res.render("home", { pageTitle: "Home" });
+export const search = (req, res) =>
+  res.render("search", { pageTitle: "Search" });
+export const logout = (req, res) =>
+  res.render("logout", { pageTitle: "Logout" });
+export const userDetail = (req, res) =>
+  res.render("userDetail", { pageTitle: "Video Detail" });
+export const editProfile = (req, res) =>
+  res.render("editProfile", { pageTitle: "Edit Video" });
+export const deleteVideo = (req, res) =>
+  res.render("changePassword", { pageTitle: "Delete Video" });
