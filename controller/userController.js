46a2a22413e03b7f4aaec471a72c7192714aa4ed
ohnewdev <ohnewdev@gmail.com>
2020-02-27 05:20:03 +0900

    add pug

--- controller/userController.js
+++ controller/userController.js
@@ -1,3 +1,10 @@
-export const join = (req, res) => res.send("join");
-export const login = (req, res) => res.send("login");
-export const logout = (req, res) => res.send("logout");
+export const join = (req, res) => res.render("join", { pageTitle: "Join" });
+export const login = (req, res) => res.render("login", { pageTitle: "Login" });
+export const logout = (req, res) =>
+  res.render("logout", { pageTitle: "Log out" });
+export const userDetail = (req, res) =>
+  res.render("userDetail", { pageTitle: "User detail" });
+export const editProfile = (req, res) =>
+  res.render("editProfile", { pageTitle: "Edit Profile" });
+export const changePassword = (req, res) =>
+  res.render("changePassword", { pageTitle: "Change Password" });
--- controller/videoController.js
+++ controller/videoController.js
@@ -1,2 +1,11 @@
-export const home = (req, res) => res.send("Home at Video");
-export const search = (req, res) => res.send("Search");
+export const home = (req, res) => res.render("home", { pageTitle: "Home" });
+export const search = (req, res) =>
+  res.render("search", { pageTitle: "Search" });
+export const logout = (req, res) =>
+  res.render("logout", { pageTitle: "Logout" });
+export const userDetail = (req, res) =>
+  res.render("userDetail", { pageTitle: "Video Detail" });
+export const editProfile = (req, res) =>
+  res.render("editProfile", { pageTitle: "Edit Video" });
+export const deleteVideo = (req, res) =>
+  res.render("changePassword", { pageTitle: "Delete Video" });
